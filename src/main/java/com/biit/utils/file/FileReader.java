636e06093be4d6f505c0d836af4465d53deb7939
Jorge Hortelano <jhortelano@biit-solutions.com>
2013-08-16 17:32:27 +0200

    FileReader and Properties File Readed created.

---
+++ src/main/java/com/biit/utils/file/FileReader.java
@@ -0,0 +1,50 @@
+package com.biit.utils.file;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+public class FileReader {
+	private final static String ICON_FOLDER = "icons";
+
+	public static File getResource(String filename) throws NullPointerException {
+		URL url = FileReader.class.getClassLoader().getResource(filename);
+		File file = null;
+		// Jetty load resource.
+		try {
+			file = new File(FileReader.convert2OsPath(url));
+			// Apache load resource
+			if (!file.exists()) {
+				try {
+					file = new File(url.toURI());
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} catch (NullPointerException npe) {
+			throw new NullPointerException("File '" + filename + "' does not exist.");
+		}
+		return file;
+	}
+
+	public static String convert2OsPath(URL string) {
+		try {
+			if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+				return (string.getPath()).substring(1);
+			} else {
+				return (string.getPath());
+			}
+		} catch (NullPointerException npe) {
+			return null;
+		}
+	}
+
+	public static ImageIcon getIcon(String iconFile) {
+		return new ImageIcon(FileReader.class.getClassLoader().getResource(
+				File.separator + ICON_FOLDER + File.separator + iconFile));
+	}
+
+}
