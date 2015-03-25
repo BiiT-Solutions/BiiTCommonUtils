ea464774d18029649cac3a5b6121e9b6179c1339
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-03-25 13:11:12 +0100

    Nullpointer avoided in convertToArray.

--- src/main/java/com/biit/utils/string/StringConverter.java
+++ src/main/java/com/biit/utils/string/StringConverter.java
@@ -2,6 +2,9 @@ package com.biit.utils.string;
 
 public class StringConverter {
 	public static String[] convertToArray(String commaSeparatedString) {
+		if (commaSeparatedString == null || commaSeparatedString.length() == 0) {
+			return new String[0];
+		}
 		String[] arrayNotFormatted = commaSeparatedString.split(",");
 		String[] formattedArray = new String[arrayNotFormatted.length];
 		for (int i = 0; i < arrayNotFormatted.length; i++) {
