7c918fabe4e145af775b8c0e88bee7813c9d10a0
aperez <aperez@biit-solutions.com>
2015-03-30 16:29:51 +0200

    Added new function to read file as a complete string

--- src/main/java/com/biit/utils/file/FileReader.java
+++ src/main/java/com/biit/utils/file/FileReader.java
@@ -2,8 +2,12 @@ package com.biit.utils.file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -90,6 +94,18 @@ public class FileReader {
 		}
 		s.close();
 		return lines;
+	}
+
+	/**
+	 * Reads a resource text file and returns String
+	 * 
+	 * @param resourceName
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
 	}
 
 }
