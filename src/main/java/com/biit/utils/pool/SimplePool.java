7e8bd3ee2e2132c5f4b567e5c4ef40c21debc8ea
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-11-26 11:44:38 +0100

    Pool only by id

--- src/main/java/com/biit/utils/pool/SimplePool.java
+++ src/main/java/com/biit/utils/pool/SimplePool.java
@@ -1,19 +1,13 @@
 package com.biit.utils.pool;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 public abstract class SimplePool<ElementId, Type extends PoolElement<ElementId>> {
 	// Elements by id;
 	private Map<ElementId, Long> elementsTime; // user id -> time.
 	private Map<ElementId, Type> elementsById;
-
-	// Classification by string.
-	private Map<String, Long> elementsTagTime; // tag -> time.
-	private Map<String, Set<Type>> elementsByTag;
 
 	public SimplePool() {
 		reset();
@@ -22,31 +16,6 @@ public abstract class SimplePool<ElementId, Type extends PoolElement<ElementId>>
 	public void addElement(Type element) {
 		elementsTime.put(element.getId(), System.currentTimeMillis());
 		elementsById.put(element.getId(), element);
-	}
-
-	public void addElementByTag(Set<Type> elements, String tag) {
-		if (tag != null && elements != null) {
-			elementsTagTime.put(tag, System.currentTimeMillis());
-			Set<Type> existingGroups = elementsByTag.get(tag);
-			if (existingGroups == null) {
-				existingGroups = new HashSet<Type>();
-			}
-			existingGroups.addAll(elements);
-			elementsByTag.put(tag, existingGroups);
-		}
-	}
-
-	public void addElementByTag(Type element, String tag) {
-		if (tag != null && element != null) {
-			addElement(element);
-			elementsTagTime.put(tag, System.currentTimeMillis());
-			Set<Type> elements = elementsByTag.get(tag);
-			if (elements == null) {
-				elements = new HashSet<Type>();
-			}
-			elements.add(element);
-			elementsByTag.put(tag, elements);
-		}
 	}
 
 	/**
@@ -83,27 +52,6 @@ public abstract class SimplePool<ElementId, Type extends PoolElement<ElementId>>
 		return elementsById;
 	}
 
-	public Set<Type> getElementsByTag(String tag) {
-		long now = System.currentTimeMillis();
-		String nextGroupTag = null;
-		if (elementsTagTime.size() > 0) {
-			Iterator<String> e = new HashMap<String, Set<Type>>(elementsByTag).keySet().iterator();
-			while (e.hasNext()) {
-				nextGroupTag = e.next();
-				if (elementsTagTime.get(nextGroupTag) != null && (now - elementsTagTime.get(nextGroupTag)) > getExpirationTime()) {
-					// object has expired
-					removeElementsByTag(nextGroupTag);
-					nextGroupTag = null;
-				} else {
-					if (tag.equals(nextGroupTag)) {
-						return elementsByTag.get(nextGroupTag);
-					}
-				}
-			}
-		}
-		return null;
-	}
-
 	public Map<ElementId, Long> getElementsTime() {
 		return elementsTime;
 	}
@@ -115,26 +63,9 @@ public abstract class SimplePool<ElementId, Type extends PoolElement<ElementId>>
 		}
 	}
 
-	public void removeElementsByTag(String tag) {
-		if (tag != null) {
-			elementsByTag.remove(tag);
-			elementsTagTime.remove(tag);
-		}
-	}
-
-	public void removeElementsByTag(String tag, PoolElement<ElementId> element) {
-		if (tag != null) {
-			if (elementsByTag.get(tag) != null) {
-				elementsByTag.get(tag).remove(element);
-			}
-		}
-	}
-
 	public void reset() {
 		elementsTime = new HashMap<ElementId, Long>();
 		elementsById = new HashMap<ElementId, Type>();
-		elementsTagTime = new HashMap<String, Long>();
-		elementsByTag = new HashMap<String, Set<Type>>();
 	}
 
 	public abstract long getExpirationTime();
