68c3ef111c082146ccb096b6dd74a72bbb49e37d
Jorge Hortelano <jhortelano@biit-solutions.com>
2017-08-11 10:09:41 +0200

    Merged with Usmo DateManager

--- src/main/java/com/biit/utils/date/DateManager.java
+++ src/main/java/com/biit/utils/date/DateManager.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateManager {
 	public final static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
@@ -51,6 +52,47 @@ public class DateManager {
 	public static Date getDate(String date, String format) throws ParseException {
 		SimpleDateFormat formatter = new SimpleDateFormat(format);
 		return formatter.parse(date);
+	}
+	
+	public static String convertDateToString(Date date, String dateFormat, Locale locale) {
+		if (locale == null) {
+			locale = Locale.ENGLISH;
+		}
+		if (date == null) {
+			return new SimpleDateFormat(dateFormat, locale).format(new Timestamp(new java.util.Date(0).getTime()));
+		}
+		return new SimpleDateFormat(dateFormat, locale).format(date);
+	}
+
+	public static String convertDateToString(Timestamp timestamp, String dateFormat, Locale locale) {
+		if (locale == null) {
+			locale = Locale.ENGLISH;
+		}
+		if (timestamp == null) {
+			return new SimpleDateFormat(dateFormat, locale).format(new Timestamp(0));
+		}
+		return new SimpleDateFormat(dateFormat, locale).format(timestamp);
+	}
+
+	public static String convertDateToStringWithHours(Date date) {
+		return convertDateToString(date, DATE_FORMAT, null);
+	}
+
+	public static String convertDateToString(Date date) {
+		return convertDateToString(date, DATE_FORMAT_SIMPLE, null);
+	}
+
+	public static String convertDateToString(Timestamp time, Locale locale) {
+		Date date = new Date(time.getTime());
+		return convertDateToString(date, DATE_FORMAT_SIMPLE, locale);
+	}
+
+	public static String formatStringDate(String date, String originFormat, String destinyFormat) throws ParseException {
+		return convertDateToString(new Date(new SimpleDateFormat(originFormat).parse(date).getTime()), destinyFormat, null);
+	}
+
+	public static String formatStringDate(Timestamp date, String destinyFormat) throws ParseException {
+		return convertDateToString(new Date(date.getTime()), destinyFormat, null);
 	}
 
 }
