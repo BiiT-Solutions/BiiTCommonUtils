c4799f80237bdbc546f75a79d47103f75cf69d4b
Jorge Hortelano <jhortelano@biit-solutions.com>
2017-12-14 10:27:04 +0100

    Added sorted properties

---
+++ src/main/java/com/biit/utils/configuration/SortedProperties.java
@@ -0,0 +1,34 @@
+package com.biit.utils.configuration;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+
+/**
+ * Properties file where all keys are sorted.
+ *
+ */
+public class SortedProperties extends Properties {
+	private static final long serialVersionUID = 1965770488142804900L;
+
+	@Override
+	public Enumeration<Object> keys() {
+		Enumeration<Object> keysEnum = super.keys();
+		Vector<Object> keyList = new Vector<Object>();
+
+		while (keysEnum.hasMoreElements()) {
+			keyList.add(keysEnum.nextElement());
+		}
+
+		Collections.sort(keyList, new Comparator<Object>() {
+			@Override
+			public int compare(Object o1, Object o2) {
+				return o1.toString().compareTo(o2.toString());
+			}
+		});
+
+		return keyList.elements();
+	}
+}
