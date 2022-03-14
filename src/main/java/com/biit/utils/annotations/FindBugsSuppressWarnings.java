80a1a1d09164f0795d1c79cb8a7c717849fa9cad
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-03-14 15:39:22 +0100

    Added Findbugs annotation

---
+++ src/main/java/com/biit/utils/annotations/FindBugsSuppressWarnings.java
@@ -0,0 +1,12 @@
+package com.biit.utils.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE})
+public @interface FindBugsSuppressWarnings {
+    String[] value();
+}
