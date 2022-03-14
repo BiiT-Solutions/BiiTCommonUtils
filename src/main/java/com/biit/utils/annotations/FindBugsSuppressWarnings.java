cb14b8e47ef4a6bd99ea33b1ba6ee7a927d8d832
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-03-14 16:02:08 +0100

    Added Findbugs annotation

--- src/main/java/com/biit/utils/annotations/FindBugsSuppressWarnings.java
+++ src/main/java/com/biit/utils/annotations/FindBugsSuppressWarnings.java
@@ -5,8 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.CLASS)
+/**
+ * Suppress FindBugs warnings on the annotated element. FindBugs will recognize
+ * any annotation that has class retention and whose name ends with
+ * "SuppressWarnings".
+ *
+ * @hide
+ */
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE})
+@Retention(RetentionPolicy.CLASS)
 public @interface FindBugsSuppressWarnings {
+    /**
+     * The <a href="http://findbugs.sourceforge.net/bugDescriptions.html">FindBugs
+     * Patterns</a> to suppress, such as {@code SE_TRANSIENT_FIELD_NOT_RESTORED}
+     * or {@code Se}. Full, upper case names are preferred.
+     */
     String[] value();
 }
